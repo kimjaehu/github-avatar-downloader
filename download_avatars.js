f4ce97c26ea40cb4f1ca814519fbb631060f17bd
Jay Kim <kimjaehu@gmail.com>
2019-02-11 22:58:31 +0000

    downloaded hardcoded avatar

--- download_avatars.js
+++ download_avatars.js
@@ -2,45 +2,52 @@ var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets.js');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+// function getRepoContributors(repoOwner, repoName, cb) {
 
-    var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-        headers: {
-          'User-Agent': 'request',
-          'Authorization': secrets.GITHUB_TOKEN
-        }
-      };
-    request(options,cb);
-}
-
-function handleResponse(err, res, body) {
-
-    if (!err && res.statusCode === 200) {
-        var apiBody = JSON.parse(body);
-        apiBody.forEach(function(url){
-            console.log(url.avatar_url)
-        });
-    } else {
-        console.log('error')
-    }
-}
-
-// function downloadImageByURL(url, filePath) {
-//     request.get('https://sytantris.github.io/http-examples/future.jpg')
-//     .on('error', function(err){
-//         throw err;
-//     })
-//     .on('response',function(response){
-//      console.log('Response Status Code: ', response.statusCode); 
-//      console.log(response.statusMessage)
-//      console.log(response.headers['content-type']);
-//      console.log('Downloading image...');
-//     })
-//     .on('end', function(){
-//     console.log('Cownload complete.')
-//     })
-//     .pipe(fs.createWriteStream('./future.jpg'));
+//     var options = {
+//         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//         headers: {
+//           'User-Agent': 'request',
+//           'Authorization': secrets.GITHUB_TOKEN
+//         }
+//       };
+//     request(options,cb);
 // }
 
-// getRepoContributors("jquery", "jquery", handleResponse);
+// function handleResponse(err, res, body) {
+
+//     if (!err && res.statusCode === 200) {
+//         var apiBody = JSON.parse(body);
+//         apiBody.forEach(function(url){
+//             console.log(url.avatar_url)
+//         });
+//     } else {
+//         console.log('error')
+//     }
+// }
+
+function downloadImageByURL(url, filePath) {
+    request.get(url + filePath)
+    .on('error', function(err){
+        throw err;
+    })
+    .on('response',function(response){
+     console.log('Response Status Code: ', response.statusCode); 
+     console.log(response.statusMessage)
+     console.log(response.headers['content-type']);
+     console.log('Downloading image...');
+    })
+    .on('end', function(){
+    console.log('Download complete.')
+    })
+    .pipe(fs.createWriteStream(filePath),function (filePath){
+        if (!fs.existsSync(filePath)){
+            fs.mkdirSync(filePath);
+        }
+    });
+        
+}
+
+//getRepoContributors("jquery", "jquery", handleResponse);
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
